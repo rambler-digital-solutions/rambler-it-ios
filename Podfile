0c5783abc5cd6fcc4b8dd507d85a863fad18008f
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-09-30 09:30:12 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://gitlab.rambler.ru/cocoapods/cocoapods.git'
+source 'https://github.com/cocoapods/Specs.git'
+
+platform :ios, '7.0'
+inhibit_all_warnings!
---
+++ conferences/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Conferences
+//
+//  Created by Egor Tolstoy on 30/09/15.
+//  Copyright © 2015 Rambler. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
