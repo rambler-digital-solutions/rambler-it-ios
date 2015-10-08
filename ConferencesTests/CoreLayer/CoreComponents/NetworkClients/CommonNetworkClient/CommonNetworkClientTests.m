fae3d6cec815d13f2bc62630cd9e2be7c6a7d0ca
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-10-08 22:36:59 +0300

    Добавлены недостающие тесты сетевого клиента

--- ConferencesTests/CoreLayer/CoreComponents/NetworkClients/CommonNetworkClient/CommonNetworkClientTests.m
+++ ConferencesTests/CoreLayer/CoreComponents/NetworkClients/CommonNetworkClient/CommonNetworkClientTests.m
@@ -9,6 +9,9 @@
 #import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
 
+#import "XCTestCase+RCFHelpers.h"
+#import "TestConstants.h"
+
 #import "CommonNetworkClient.h"
 
 static NSString *const kTestURLPath = @"https://myapi.com/v1/rest";
@@ -48,6 +51,60 @@ static NSString *const kTestURLPath = @"https://myapi.com/v1/rest";
     OCMVerify([self.mockSession dataTaskWithRequest:request
                                   completionHandler:OCMOCK_ANY]);
 }
+
+- (void)testThatClientCallsCompletionBlockWithData {
+    // given
+    XCTestExpectation *expectation = [self expectationForCurrentTest];
+    NSURLRequest *request = [self generatePreconfiguredRequest];
+    
+    NSData *testData = [NSData new];
+    
+    [self stubSessionToAutocompleteWithRequest:request
+                                          data:testData
+                                         error:nil];
+    __block id resultData;
+    
+    // when
+    [self.client sendRequest:request
+             completionBlock:^(NSData *data, NSError *error) {
+        resultData = data;
+        [expectation fulfill];
+    }];
+    
+    // then
+    [self waitForExpectationsWithTimeout:kTestExpectationTimeout
+                                 handler:^(NSError * _Nullable error) {
+        XCTAssertEqualObjects(resultData, testData);
+    }];
+}
+
+- (void)testThatClientCallsCompletionBlockWithError {
+    // given
+    XCTestExpectation *expectation = [self expectationForCurrentTest];
+    NSURLRequest *request = [self generatePreconfiguredRequest];
+    
+    NSError *clientError = [NSError errorWithDomain:@"Test Domain"
+                                               code:0
+                                           userInfo:nil];
+    
+    [self stubSessionToAutocompleteWithRequest:request
+                                          data:nil
+                                         error:clientError];
+    __block id resultError;
+    
+    // when
+    [self.client sendRequest:request
+             completionBlock:^(NSData *data, NSError *error) {
+                 resultError = error;
+                 [expectation fulfill];
+             }];
+    
+    // then
+    [self waitForExpectationsWithTimeout:kTestExpectationTimeout
+                                 handler:^(NSError * _Nullable error) {
+                                     XCTAssertEqualObjects(resultError, clientError);
+                                 }];
+}
                              
 #pragma mark - Helper Methods
                              
@@ -55,6 +112,19 @@ static NSString *const kTestURLPath = @"https://myapi.com/v1/rest";
     NSURL *targetURL = [NSURL URLWithString:kTestURLPath];
     NSURLRequest *request = [NSURLRequest requestWithURL:targetURL];
     return request;
+}
+
+- (void)stubSessionToAutocompleteWithRequest:(NSURLRequest *)request
+                                                 data:(NSData *)data
+                                                error:(NSError *)error {
+    ProxyBlock proxyBlock = ^(NSInvocation *invocation) {
+        void (^completionBlock)(NSData * __nullable data, NSURLResponse * __nullable response, NSError * __nullable error);
+        [invocation getArgument:&completionBlock atIndex:3];
+        
+        completionBlock(data, nil, error);
+    };
+    OCMStub([self.mockSession dataTaskWithRequest:request
+                                completionHandler:OCMOCK_ANY]).andDo(proxyBlock);
 }
 
 @end
