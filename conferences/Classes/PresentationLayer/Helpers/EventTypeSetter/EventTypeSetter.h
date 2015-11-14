1abbb7fc30fb9fc6327c33cd093c1191f1dbaa55
Karpushin Artem <a.karpushin@rambler-co.ru>
2015-11-14 19:06:34 +0300

    Добавлен вспомогательный класс EventTypeSetter

---
+++ conferences/Classes/PresentationLayer/Helpers/EventTypeSetter/EventTypeSetter.h
@@ -0,0 +1,13 @@
+//
+//  EventTypeSetter.h
+//  Conferences
+//
+//  Created by Karpushin Artem on 14/11/15.
+//  Copyright © 2015 Rambler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EventTypeSetter : NSObject
+
+@end
---
+++ conferences/Classes/PresentationLayer/Helpers/EventTypeSetter/EventTypeSetter.m
@@ -0,0 +1,13 @@
+//
+//  EventTypeSetter.m
+//  Conferences
+//
+//  Created by Karpushin Artem on 14/11/15.
+//  Copyright © 2015 Rambler. All rights reserved.
+//
+
+#import "EventTypeSetter.h"
+
+@implementation EventTypeSetter
+
+@end
