202c586600316027b7f9804f409534b2f56220f4
Karpushin Artem <a.karpushin@rambler-co.ru>
2015-11-14 19:11:33 +0300

    Добавлена фабрика  PresentationLayerHelpersAssembly

---
+++ conferences/Classes/PresentationLayer/Helpers/Assembly/PresentationLayerHelpersAssembly.h
@@ -0,0 +1,22 @@
+//
+//  PresentationLayerHelpersAssembly.h
+//  Conferences
+//
+//  Created by Karpushin Artem on 14/11/15.
+//  Copyright © 2015 Rambler. All rights reserved.
+//
+
+#import <Typhoon/Typhoon.h>
+
+@class EventTypeSetter;
+
+/**
+ @author Artem Karpushin
+ 
+ A TyphoonAssembly which is responsible for creating helper objects for presentation layer
+ */
+@interface PresentationLayerHelpersAssembly : TyphoonAssembly
+
+- (EventTypeSetter *)eventTypeSetter;
+
+@end
