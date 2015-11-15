3eae9aa10dc02ae73439fb08305e59fd3c17d9da
Karpushin Artem <a.karpushin@rambler-co.ru>
2015-11-15 11:03:44 +0300

    Добавлена модель PlainLecture

--- conferences/Classes/Models/PlainObjects/PlainLecture.h
+++ conferences/Classes/Models/PlainObjects/PlainLecture.h
@@ -10,4 +10,15 @@
 
 @interface PlainLecture : NSObject
 
+// пока окончательно не сформирована модель данных - все проперти readwrite
+
+@property (strong, nonatomic, readwrite) NSNumber *favourite;
+@property (strong, nonatomic, readwrite) NSNumber *name;
+@property (strong, nonatomic, readwrite) NSNumber *orderID;
+@property (strong, nonatomic, readwrite) NSString *slideLink;
+@property (strong, nonatomic, readwrite) NSString *lectureDescription;
+@property (strong, nonatomic, readwrite) NSString *videoLink;
+@property (strong, nonatomic, readwrite) NSArray *lectureMaterials;
+@property (strong, nonatomic, readwrite) NSArray *speakers;
+
 @end
