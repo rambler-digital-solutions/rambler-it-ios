296ec0b2fca1b58542ceda1bded382cc0fdf85c3
Karpushin Artem <a.karpushin@rambler-co.ru>
2015-11-22 17:42:16 +0300

    Added ReportListDataDisplayManager tests

---
+++ ConferencesTests/ConferencesPresentationTests/UserStories/ReportsUserStoryTests/ReportListModuleTests/ReportListDataDisplayManagerTests.m
@@ -0,0 +1,120 @@
+//
+//  ReportListDataDisplayManagerTests.m
+//  Conferences
+//
+//  Created by Karpushin Artem on 22/11/15.
+//  Copyright © 2015 Rambler. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
+
+#import "ReportListDataDisplayManager.h"
+#import "ReportListTableViewCell.h"
+#import "PlainEvent.h"
+#import "ReportListTableViewController.h"
+
+typedef NS_ENUM(NSUInteger, TableViewSectionIndex){
+    ReportsSection = 0
+};
+
+@interface ReportListDataDisplayManagerTests : XCTestCase
+
+@property (strong, nonatomic) ReportListDataDisplayManager *dataDisplayManager;
+@property (strong, nonatomic) NSArray *events;
+
+@end
+
+@implementation ReportListDataDisplayManagerTests
+
+- (void)setUp {
+    [super setUp];
+    
+    self.dataDisplayManager = [ReportListDataDisplayManager new];
+    self.events = @[[PlainEvent new], [PlainEvent new], [PlainEvent new]];
+}
+
+- (void)tearDown {
+    self.dataDisplayManager = nil;
+    self.events = nil;
+    
+    [super tearDown];
+}
+
+- (void)testThatDataDisplayManagerReturnsTableViewDataSource{
+    // given
+    
+    // when
+    [self.dataDisplayManager updateTableViewModelWithEvents:self.events];
+    id <UITableViewDataSource> dataSource = [self.dataDisplayManager dataSourceForTableView:nil];
+    
+    // then
+    XCTAssertNotNil(dataSource);
+}
+
+- (void)testSuccessUpdateTableViewModelWithEvents {
+    // given
+    id mockViewController = OCMClassMock([ReportListTableViewController class]);
+    self.dataDisplayManager.delegate = mockViewController;
+    
+    // when
+    [self.dataDisplayManager updateTableViewModelWithEvents:self.events];
+    
+    // then
+    OCMVerify([mockViewController didUpdateTableViewModel]);
+}
+
+- (void)testThatDataDisplayManagerReturnsCorrectNumberOfSections {
+    // given
+    NSUInteger const kExpectedNumberOfSections = 1;
+    NSUInteger actualNumberOfSections = 0;
+    
+    // when
+    [self.dataDisplayManager updateTableViewModelWithEvents:self.events];
+    id <UITableViewDataSource> dataSource = [self.dataDisplayManager dataSourceForTableView:nil];
+    
+    actualNumberOfSections = [dataSource numberOfSectionsInTableView:nil];
+    
+    // then
+    XCTAssertEqual(actualNumberOfSections, kExpectedNumberOfSections);
+}
+
+- (void)testThatDataDisplayManagerReturnsCorrectNumberOfRows {
+    // given
+    NSUInteger const kExpectedNumberOfRows = self.events.count;
+    NSUInteger actualNumberOfRows = 0;
+    
+    // when
+    [self.dataDisplayManager updateTableViewModelWithEvents:self.events];
+    id <UITableViewDataSource> dataSource = [self.dataDisplayManager dataSourceForTableView:nil];
+    actualNumberOfRows = [dataSource tableView:nil numberOfRowsInSection:ReportsSection];
+    
+    // then
+    XCTAssertEqual(kExpectedNumberOfRows, actualNumberOfRows);
+}
+
+- (void)testThatDataDisplayManagerReturnsCorrectClassForTableViewCell {
+    // given
+    NSUInteger expectedNumberOfCellForCorrespondingClass = self.events.count;
+    NSUInteger actualNumberOfCellForCorrespondingClass = 0;
+    
+    // when
+    [self.dataDisplayManager updateTableViewModelWithEvents:self.events];
+    id <UITableViewDataSource> dataSource = [self.dataDisplayManager dataSourceForTableView:nil];
+    
+    NSUInteger numberOfRows = [dataSource tableView:nil numberOfRowsInSection:ReportsSection];
+    
+    for (int i = 0; i < numberOfRows; i++) {
+        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:ReportsSection];
+        UITableViewCell *cell = [dataSource tableView:nil cellForRowAtIndexPath:indexPath];
+        
+        if ([cell isKindOfClass:[ReportListTableViewCell class]]) {
+            actualNumberOfCellForCorrespondingClass++;
+        }
+    }
+    
+    // then
+    XCTAssertEqual(expectedNumberOfCellForCorrespondingClass, actualNumberOfCellForCorrespondingClass);
+}
+
+@end
