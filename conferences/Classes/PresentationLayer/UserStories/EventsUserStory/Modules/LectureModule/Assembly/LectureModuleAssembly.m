5227292b8f2e64fc136b68cbc154a1c3593bb112
Karpushin Artem <a.karpushin@rambler-co.ru>
2016-01-06 16:24:58 +0300

    Added base module

---
+++ conferences/Classes/PresentationLayer/UserStories/EventsUserStory/Modules/LectureModule/Assembly/LectureModuleAssembly.m
@@ -0,0 +1,77 @@
+// Copyright (c) 2016 RAMBLER&Co
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "LectureModuleAssembly.h"
+#import "LectureViewController.h"
+#import "LectureInteractor.h"
+#import "LecturePresenter.h"
+#import "LectureRouter.h"
+#import "LectureDataDisplayManager.h"
+#import "LecturePresenterStateStorage.h"
+
+@implementation  LectureModuleAssembly
+
+- (LectureViewController *)viewLecture {
+    return [TyphoonDefinition withClass:[LectureViewController class]
+                            configuration:^(TyphoonDefinition *definition) {
+                                [definition injectProperty:@selector(output)
+                                                      with:[self presenterLecture]];
+                                [definition injectProperty:@selector(dataDisplayManager)
+                                                      with:[self dataDisplayManagerLecture]];
+             }];
+}
+
+- (LectureInteractor *)interactorLecture {
+    return [TyphoonDefinition withClass:[LectureInteractor class]
+                            configuration:^(TyphoonDefinition *definition) {
+                                [definition injectProperty:@selector(output)
+                                                      with:[self presenterLecture]];
+             }];
+}
+
+- (LecturePresenter *)presenterLecture {
+    return [TyphoonDefinition withClass:[LecturePresenter class]
+                            configuration:^(TyphoonDefinition *definition) {
+                                [definition injectProperty:@selector(view)
+                                                      with:[self viewLecture]];
+                                [definition injectProperty:@selector(interactor)
+                                                      with:[self interactorLecture]];
+                                [definition injectProperty:@selector(router)
+                                                      with:[self routerLecture]];
+                                [definition injectProperty:@selector(stateStorage)
+                                                      with:[self presenterStateStorageLecture]];
+            }];
+}
+
+- (LectureRouter *)routerLecture {
+    return [TyphoonDefinition withClass:[LectureRouter class]
+                            configuration:^(TyphoonDefinition *definition) {
+           }];
+}
+
+- (LecturePresenterStateStorage *)presenterStateStorageLecture {
+    return [TyphoonDefinition withClass:[LecturePresenterStateStorage class]];
+}
+
+- (LectureDataDisplayManager *)dataDisplayManagerLecture {
+    return [TyphoonDefinition withClass:[LectureDataDisplayManager class]];
+}
+
+@end
