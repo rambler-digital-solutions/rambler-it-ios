95ae6cb5119614450920ee62b7b4f984a9c70bf4
Karpushin Artem <a.karpushin@rambler-co.ru>
2016-01-16 20:10:00 +0300

    Removed unnecessary code

--- conferences/Classes/PresentationLayer/UserStories/EventsUserStory/Modules/LectureModule/View/DataDisplayManager/LectureDataDisplayManager.m
+++ conferences/Classes/PresentationLayer/UserStories/EventsUserStory/Modules/LectureModule/View/DataDisplayManager/LectureDataDisplayManager.m
@@ -66,9 +66,6 @@
 - (NITableViewModel *)configureTableViewModel {
     NSMutableArray *cellObjects = [@[] mutableCopy];
     
-    TableViewCellWithTextLabelCellObject *descriptionCellObject = [TableViewCellWithTextLabelCellObject objectWithText:@"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."];
-    [cellObjects addObject:descriptionCellObject];
-    
     NITableViewModel *tableViewModel = [[NITableViewModel alloc] initWithSectionedArray:cellObjects
                                                                                              delegate:(id)[NICellFactory class]];
     return tableViewModel;
