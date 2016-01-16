b747dc7741b23ddd992fd4ff16ebffc4bbf0552e
Karpushin Artem <a.karpushin@rambler-co.ru>
2016-01-16 15:04:14 +0300

    Added StringConstants

---
+++ conferences/Classes/Constants/StringConstants/StringConstants.h
@@ -0,0 +1,21 @@
+//
+//  Constants.h
+//  Conferences
+//
+//  Created by Karpushin Artem on 16/01/16.
+//  Copyright © 2016 Rambler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StringConstants : NSObject
+
+#pragma mark - Cell title
+
+extern NSString *const VideoRecordTableViewCellTitle;
+extern NSString *const LectureMaterialsTableViewCellTitle;
+extern NSString *const AdditionInformationTableViewCellTitle;
+extern NSString *const SpeakerWebSiteTableViewCellTitle;
+extern NSString *const SpeakerContactsTableViewCellTitle;
+
+@end
