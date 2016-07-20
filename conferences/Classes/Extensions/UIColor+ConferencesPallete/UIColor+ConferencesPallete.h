5f89e5a2042f2a383473bf0f2fc1c178cc9e1e5c
k.zinovyev <k.zinovyev@rambler-co.ru>
2016-07-20 13:15:04 +0300

    Add UIColor category with ConferencesPallete

---
+++ conferences/Classes/Extensions/UIColor+ConferencesPallete/UIColor+ConferencesPallete.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+ConferencesPallete.h
+//  Conferences
+//
+//  Created by k.zinovyev on 19.07.16.
+//  Copyright © 2016 Rambler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ConferencesPallete)
+
++ (UIColor *)colorForSelectedTextEventCellObject;
++ (UIColor *)colorForSelectedTextLectureCellObject;
++ (UIColor *)colorForSelectedTextSpeakerCellObject;
+
+@end
