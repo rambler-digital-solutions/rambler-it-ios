fb31abc378024deeb2ea52deaf4cbe4c6485b45a
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-08-16 18:15:30 +0300

    Fixed incorrect time format (Issue #97)

--- conferences/Classes/PresentationLayer/Helpers/DateFormatter/DateFormatter.m
+++ conferences/Classes/PresentationLayer/Helpers/DateFormatter/DateFormatter.m
@@ -20,12 +20,12 @@
 
 #import "DateFormatter.h"
 
-static NSString *const DayMonthTimeDateFormat = @"d MMMM в hh:mm";
+static NSString *const DayMonthTimeDateFormat = @"d MMMM в HH:mm";
 static NSString *const DayMonthYearDateFormat = @"d MMMM yyyy";
 static NSString *const DayMonthDateFormat = @"d MMMM";
 static NSString *const MonthDateFormat = @"MMMM";
 static NSString *const DayDateFormat = @"d";
-static NSString *const TimeDateFormat = @"hh:mm";
+static NSString *const TimeDateFormat = @"HH:mm";
 
 @implementation DateFormatter
 
