9e5e1e31267fb66407787657ccf38764c6484470
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2016-08-17 17:15:27 +0300

    Added ShareUrlBuilder for building sharing urls

---
+++ ConferencesTests/ConferencesPresentationTests/HelpersTests/ShareUrlBuilderTests/ShareUrlBuilderImplementationTests.m
@@ -0,0 +1,59 @@
+// Copyright (c) 2015 RAMBLER&Co
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <XCTest/XCTest.h>
+
+#import "ShareUrlBuilderImplementation.h"
+
+static NSString *const kTestItemType = @"item";
+
+@interface ShareUrlBuilderImplementationTests : XCTestCase
+
+@property (nonatomic, strong) ShareUrlBuilderImplementation *builder;
+
+@end
+
+@implementation ShareUrlBuilderImplementationTests
+
+- (void)setUp {
+    [super setUp];
+    
+    self.builder = [[ShareUrlBuilderImplementation alloc] initWithItemType:kTestItemType];
+}
+
+- (void)tearDown {
+    self.builder = nil;
+    
+    [super tearDown];
+}
+
+- (void)testThatBuilderBuildsProperUrl {
+    // given
+    NSString *const kTestItemId = @"1234";
+    NSString *const kExpectedUrlString = @"http://it.rambler-co.ru/#/item/1234";
+    
+    // when
+    NSURL *result = [self.builder buildShareUrlWithItemId:kTestItemId];
+    
+    // then
+    XCTAssertEqualObjects(result.absoluteString, kExpectedUrlString);
+}
+
+@end
