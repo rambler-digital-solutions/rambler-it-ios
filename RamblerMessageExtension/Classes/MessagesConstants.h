91aebfebf3004b6cbb7f8a6628449e2dfba89f49
Trishina Ekaterina <etrishina@mac488RIH.local>
2016-11-01 11:40:35 +0300

    вынесен файл с констанами

---
+++ RamblerMessageExtension/Classes/MessagesConstants.h
@@ -0,0 +1,23 @@
+//
+//  MessagesConstants.h
+//  Conferences
+//
+//  Created by Trishina Ekaterina on 01/11/16.
+//  Copyright © 2016 Rambler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString * const RCFAppGroupIdentifier;
+extern NSString * const RCFCoreDataNameKey;
+extern NSString * const EventControllerIdentifier;
+
+extern NSString * const RCFURLScheme;
+extern NSString * const RCFURLHostName;
+
+extern NSString * const RCFURLQueryItemType;
+extern NSString * const RCFURLQueryItemId;
+
+
+
---
+++ RamblerMessageExtension/Classes/MessagesConstants.m
@@ -0,0 +1,17 @@
+//
+//  MessagesConstants.m
+//  Conferences
+//
+//  Created by Trishina Ekaterina on 01/11/16.
+//  Copyright © 2016 Rambler. All rights reserved.
+//
+
+#import "MessagesConstants.h"
+
+NSString * const RCFAppGroupIdentifier = @"group.ru.ramblerco.rambler.it";
+NSString * const RCFCoreDataNameKey = @"Conferences.sqlite";
+NSString * const EventControllerIdentifier = @"EventViewController";
+NSString * const RCFURLScheme = @"ramblerconferences";
+NSString * const RCFURLHostName = @"report";
+NSString * const RCFURLQueryItemType = @"type";
+NSString * const RCFURLQueryItemId = @"id";
