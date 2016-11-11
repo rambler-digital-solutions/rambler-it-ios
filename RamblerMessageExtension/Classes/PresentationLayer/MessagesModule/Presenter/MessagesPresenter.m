ccd5a879e803b74ce007b407f10af327a5936f22
Trishina Ekaterina <etrishina@mac488RIH.local>
2016-11-11 18:23:10 +0300

    добавлена реализация презентера

--- RamblerMessageExtension/Classes/PresentationLayer/MessagesModule/Presenter/MessagesPresenter.m
+++ RamblerMessageExtension/Classes/PresentationLayer/MessagesModule/Presenter/MessagesPresenter.m
@@ -1,13 +1,51 @@
+// Copyright (c) 2015 RAMBLER&Co
 //
-//  MessagesPresenter.m
-//  Conferences
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-//  Created by Trishina Ekaterina on 11/11/16.
-//  Copyright © 2016 Rambler. All rights reserved.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "MessagesPresenter.h"
+#import <Messages/Messages.h>
+#import "MessagesViewInput.h"
+#import "MessagesInteractorInput.h"
+#import "MessagesRouterInput.h"
+#import "EventPlainObject.h"
 
 @implementation MessagesPresenter
+
+- (void)setupView {
+    [self.interactor setupCoreDataStack];
+    NSArray *events = [self.interactor obtainEventList];
+    [self.view setupViewWithEventList:events];
+}
+
+- (void)didInsertNewMessageWithEvent:(EventPlainObject *)event {
+    MSMessage *message = [self.interactor createMessageFromEvent:event];
+    [self.view setupViewWithNewMessage:message];
+}
+
+- (void)didTapMessageWith:(MSMessage *)message
+     withExtensionContext:(NSExtensionContext *)extensionContext {
+    NSString *identifier = message.URL.absoluteString;
+    BOOL isCorrectIdentifier = [self.interactor isCorrectIdentifier:identifier];
+    if (isCorrectIdentifier) {
+        [self.router openEventModuleWithIdentifier:identifier
+                               andExtensionContext:extensionContext];
+    }
+}
 
 @end
